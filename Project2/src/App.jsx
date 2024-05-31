ba681467185e7d8e573d6f6db531a26a707d8cf2
Adityasingh77777 <singhadityakumar2022@gmail.com>
2024-05-31 23:09:04 +0530

    message

---
+++ Project2/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navigation from "./components/Navigation/Navigation/Navigation";
+import ContactHeader from "./components/Navigation/ContactHeader/ContactHeader";
+
+function App() {
+
+  return <div>
+
+    <Navigation/>
+    <ContactHeader/>
+
+  </div>
+}
+
+export default App
---
+++ Project2/src/components/Navigation/ContactForm/ContactForm.jsx
@@ -0,0 +1,17 @@
+import styles from "./ContactForm.module.css";
+
+const ContactForm = () => {
+  return (
+    <section className={styles.container}>
+    <div>
+        
+    </div>
+    <div>
+      ContactForm
+    </div>
+    </section>
+  )
+}
+
+export default ContactForm
+
